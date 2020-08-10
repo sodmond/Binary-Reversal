2e6a7b0ffb519659f840d7bd3ceda5b44e98cf77
Sodik Owolabi <siresodmond@gmail.com>
2020-08-10 15:23:31 +0100

    Create solution.php

---
+++ solution.php
@@ -0,0 +1,13 @@
+function BinaryReversal($str){
+  $bin = decbin($str);
+  if (strlen($bin) < 8){
+      $bin = str_pad($bin, 8, "0", STR_PAD_LEFT);
+  }
+  $arr_bin = str_split($bin);
+  $rev_bin = implode("", array_reverse($arr_bin));
+  $result = bindec($rev_bin);
+  return $result;
+}
+
+$num = 47;
+echo BinaryReversal($num);
