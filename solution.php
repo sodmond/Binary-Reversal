debda1d7d45d77ad107e63002060e821599a2a5e
Sodik Owolabi <siresodmond@gmail.com>
2020-08-10 15:28:11 +0100

    Update solution.php
    Don't pad the binary if the length is less than 8

--- solution.php
+++ solution.php
@@ -1,7 +1,12 @@
 function BinaryReversal($str){
   $bin = decbin($str);
+  $result;
   if (strlen($bin) < 8){
       $bin = str_pad($bin, 8, "0", STR_PAD_LEFT);
+      $arr_bin = str_split($bin);
+      $rev_bin = implode("", array_reverse($arr_bin));
+      $result = bindec($rev_bin);
+      return $result;
   }
   $arr_bin = str_split($bin);
   $rev_bin = implode("", array_reverse($arr_bin));
@@ -9,5 +14,5 @@ function BinaryReversal($str){
   return $result;
 }
 
-$num = 47;
+$num = 2447;
 echo BinaryReversal($num);
